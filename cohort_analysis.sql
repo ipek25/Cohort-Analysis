5f05b9404363dc182293398a2ada4733fd3cd7e1
Ipek Kurekci <96056779+ipek25@users.noreply.github.com>
2022-03-09 12:50:50 +0300

    Update cohort_analysis.sql

--- cohort_analysis.sql
+++ cohort_analysis.sql
@@ -1,5 +1,5 @@
--- Converting Unix timestamp into real time and finding the first purchase day by taking the minimum 
---difference between install time and event time (adjusted to Turkey time zone UTC+3)
+-- Converting Unix timestamp into real time and finding the first purchase day by taking the minimum difference between install time and event time (adjusted to Turkey time zone UTC+3)
+
 CREATE TEMP TABLE first_day AS (
 	SELECT user_id, MIN(real_event_time::DATE-real_install_time::DATE) as first_purchase_day
 	FROM (
@@ -12,8 +12,8 @@ CREATE TEMP TABLE first_day AS (
 		FROM dataset) AS new
 	GROUP BY user_id);
 
---Converting Unix timestamp into real time and finding the days that players made purchase by taking  
---difference between install time and event time with revenue included
+--Converting Unix timestamp into real time and finding the days that players made purchase by taking difference between install time and event time with revenue included
+
 CREATE TEMP TABLE whole AS (
 	SELECT user_id, (real_event_time::DATE-real_install_time::DATE) as days, revenue
 	FROM (
@@ -26,6 +26,7 @@ CREATE TEMP TABLE whole AS (
 	FROM dataset) AS new);
 
 --Grouping sum of revenues generated each day by players' first purchase day
+
 SELECT first_purchase_day, days, SUM(revenue)
 	FROM whole 
 LEFT JOIN first_day ON whole.user_id=first_day.user_id
